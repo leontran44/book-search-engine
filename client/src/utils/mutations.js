a7c108b8f34e11439e7b49d46a516c6b4f8c6701
Leon <tranhoangquan44@gmail.com>
2024-11-02 00:49:50 +1100

    fix bug by import gql

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const ADD_USER = gql`
+export const CREATE_USER = gql`
 	mutation AddUser($username: String!, $email: String!, $password: String!) {
 		addUser(username: $username, email: $email, password: $password) {
 			token
